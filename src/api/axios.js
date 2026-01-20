fa0a2880353da4fe18c814f4ef8dc7ce8dcfe1da
Anshid <muhammedanshidh.t@gmail.com>
2026-01-20 17:56:56 +0530

    second

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: "https://feedback-server-euh6.onrender.com",
   withCredentials: true, // 🔥 REQUIRED FOR COOKIES
 });
 
